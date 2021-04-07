881ecc74e5efc10ec654301d3d6a44ea52a7f636
Saull Brandão <saull@outlook.com>
2021-04-07 10:54:59 -0300

    Create a BookList component

---
+++ src/components/BookList.js
@@ -0,0 +1,13 @@
+const BookList = () => {
+    return (
+        <div className='book-list'>
+            <ul>
+                <li>Clean Code</li>
+                <li>The Pragmatic Programmer</li>
+                <li>The Mythical Man-Month</li>
+            </ul>
+        </div>
+    )
+}
+
+export default BookList
