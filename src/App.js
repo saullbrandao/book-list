85428d1a8eae449ce8c75721ee2e77ecef3f2c58
Saull Brandão <saull@outlook.com>
2021-04-07 10:56:08 -0300

    Make the App render the Navbar and Booklist components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import BookList from "./components/BookList";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Navbar />
+      <BookList />
     </div>
   );
 }
