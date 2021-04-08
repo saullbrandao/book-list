56fd677a70a7704823bef8bb84edac97d4f5099b
Saull Brandão <saull@outlook.com>
2021-04-08 11:19:17 -0300

    Reestructure the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,13 @@
 import { useContext } from "react"
-import { AuthContext } from "../contexts/AuthContext"
-import { ThemeContext } from "../contexts/ThemeContext"
+import { BookContext } from "../contexts/BookContext"
 
 const Navbar = () => {
-    const { isLightTheme, light, dark } = useContext(ThemeContext)
-    const theme = isLightTheme ? light : dark
-
-    const { isAuthenticated, toggleAuth } = useContext(AuthContext)
+    const { books } = useContext(BookContext)
 
     return (
-        <nav
-            style={{
-                background: theme.ui,
-                color: theme.syntax
-            }}
-        >
-            <h1>Context App</h1>
-            <div onClick={toggleAuth}>
-                {isAuthenticated ? 'Logged in' : 'Logged out'}
-            </div>
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
+        <nav className="navbar">
+            <h1>Saull Reading List</h1>
+            <p>Currently you have {books.length} books to get through...</p>
         </nav>
     )
 }
