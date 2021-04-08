12121145c394cdb0174ee2c3bb9159ca1c670358
Saull Brandão <saull@outlook.com>
2021-04-08 10:28:22 -0300

    Use the theme and auth information provided by their respective contexts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,24 @@
+import { useContext } from "react"
+import { AuthContext } from "../contexts/AuthContext"
+import { ThemeContext } from "../contexts/ThemeContext"
+
 const Navbar = () => {
+    const { isLightTheme, light, dark } = useContext(ThemeContext)
+    const theme = isLightTheme ? light : dark
+
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext)
+
     return (
-        <nav>
+        <nav
+            style={{
+                background: theme.ui,
+                color: theme.syntax
+            }}
+        >
             <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {isAuthenticated ? 'Logged in' : 'Logged out'}
+            </div>
             <ul>
                 <li>Home</li>
                 <li>About</li>
